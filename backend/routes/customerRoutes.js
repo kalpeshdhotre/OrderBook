fb7eccee7737bf5261b63860280ad773b9f0cc80
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2024-10-22 20:00:23 +0530

    changes to frontend

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -2,8 +2,14 @@ import express from "express";
 
 const router = express.Router();
 
-router.get(`/create`, (req, res) => {
-	res.send("User created");
+// Route to create new user
+router.post("/create", (req, res) => {
+	res.send(`User created`);
+});
+
+// Route to get all users, registerd in Store.
+router.get("/get", (req, res) => {
+	res.send(`Received get user request on server`);
 });
 
 export default router;
